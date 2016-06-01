b99242d5e5b9c2ace565547917f09668635fc2e9
michaelukweb <michaelafc01@gmail.com>
2016-06-01 17:42:20 +0100

    Leap Year

---
+++ May_2015_Q2.java
@@ -0,0 +1,19 @@
+public class May_2015_Q2 {
+
+	public static void main (String [] args) {
+		
+		isLeapYear(2016);
+	}
+	
+	public static boolean isLeapYear (int a ){
+		
+		if ((a % 4 == 0) || (a % 400 == 0) && (a % 100 != 0)) {
+			
+			System.out.println("Leap year");
+			return true;
+		} else
+		
+			System.out.println("Not a leap year");
+			return false;
+	}
+}
